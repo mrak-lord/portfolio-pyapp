9da3ac2e1a4915f551b7dee346ce2b213562e5e4
Martin Bachvarov <martin.bachvarov@myoplo.co.uk>
2022-08-21 20:38:00 +0100

    bash

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 echo 'Starting deployment process...'
 echo 'Pulling changes from github repo...'
 git pull origin main
+curl -X POST -H "Content-Type: application/json" https://www.pythonanywhere.com/user/mraklord/webapps/mraklord.pythonanywhere.com/reload
