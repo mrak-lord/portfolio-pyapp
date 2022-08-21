6d82641b23f7b8ff29708beb932d8fbae30a3392
Martin Bachvarov <martin.bachvarov@myoplo.co.uk>
2022-08-21 20:59:32 +0100

    clean up- final

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-echo 'Starting deployment process...'
-echo 'Pulling changes from github repo...'
-git pull origin main
-curl -X POST -H "Content-Type: application/json" -H "X-CSRFToken: AlqsGgZykYrpRiEZqcunvmdOlgpC9rnDU55urCVoxzkb2p04UyuoIHrR7uazCyxT" https://www.pythonanywhere.com/user/mraklord/webapps/mraklord.pythonanywhere.com/reload
