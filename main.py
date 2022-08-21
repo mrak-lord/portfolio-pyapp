90a24c75bd873b86fc3fd60048a503e15e25486f
Martin Bachvarov <martin.bachvarov@myoplo.co.uk>
2022-08-21 17:29:18 +0100

    fix on template

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request, redirect
 import csv
+import json
 
 
 app = Flask(__name__)
 print(__name__)
 
+with open('/home/martin-bachvarov/scool/repos/portfolio-pyapp/profile_data.json', mode='r') as user:
+    user_json = json.load(user)
+
+
 
 @app.route('/')
 def home_page():
-    return render_template('index-2.html')
+    yo = render_template('test.html')
+    return render_template('index-2.html', name=user_json['name'], jobTitle=user_json['jobTitle'], education=user_json['education'], experience=user_json['experience'], skills=user_json['skills'], bio=user_json['bio'], origin=user_json['from'], lives=user_json['lives'], bornIn=user_json['bornIn'], gender=user_json['gender'])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])
