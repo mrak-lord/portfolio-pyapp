766f18fe33948202200bc07465a9f20d75383791
Martin Bachvarov <martin.bachvarov@myoplo.co.uk>
2022-08-21 20:45:44 +0100

    message not empty

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo 'Starting deployment process...'
 echo 'Pulling changes from github repo...'
 git pull origin main
-curl -X POST -H "Content-Type: application/json" https://www.pythonanywhere.com/user/mraklord/webapps/mraklord.pythonanywhere.com/reload
+curl -X POST -H "Content-Type: application/json" -H "X-CSRFToken: AlqsGgZykYrpRiEZqcunvmdOlgpC9rnDU55urCVoxzkb2p04UyuoIHrR7uazCyxT" https://www.pythonanywhere.com/user/mraklord/webapps/mraklord.pythonanywhere.com/reload
